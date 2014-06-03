b8f042a2918da27ebf6d02cf78c2649fde4d4473
Mike Chelen <michael.chelen@gmail.com>
2014-06-02 21:08:35 -0400

    rename & update chart

---
+++ file/8c6563f5-7230-4a97-b888-7a1536b5b746/columnChart.js
@@ -0,0 +1,15 @@
+    google.load('visualization', '1', {packages: ['corechart']});
+    function drawVisualization() {
+      google.visualization.drawChart({
+        "containerId": "visualization_div",
+        "dataSourceUrl": 'http://www.google.com/fusiontables/gvizdata?tq=',
+        "query":"select title,scopus,crossref,pubmed from 1zkfQ7rtG9UI5a8rPDk2bpD6d0QbgP63h2v2l9YzW where publication_date >= '2013-01-01 00:00:00' and publication_date < '2014-01-01 00:00:00' order by scopus desc limit 10",
+        "chartType": "ColumnChart",
+        "options": {
+          "title": "Top 10 PLoS Articles From 2013 by Scopus Citations",
+          "vAxis": {"title": "Citations"},
+          "height": 800,
+        }
+      });
+    }
+    google.setOnLoadCallback(drawVisualization);
